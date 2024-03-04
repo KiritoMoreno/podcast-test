77a9a4e83233e745a4fd2d0121396d67985553af
KiritoMoreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-03-04 17:35:03 +0000

    we created the link and new elements

--- feed.py
+++ feed.py
@@ -8,9 +8,20 @@ with open('feed.yaml', 'r') as file:
 
 
 channel_element = xml_tree.SubElement(rss_element, 'channel')
-
+# It's going to hold the URL that we created in the GitHub page
+link_prefix = yaml_data['link']
 
 xml_tree.SubElement(channel_element,'title').text = yaml_data['title']
+xml_tree.SubElement(channel_element,'format').text = yaml_data['format']
+xml_tree.SubElement(channel_element,'subtitle').text = yaml_data['subtitle']
+xml_tree.SubElement(channel_element,'itunes:author').text = yaml_data['autor']
+xml_tree.SubElement(channel_element,'description').text = yaml_data['description']
+xml_tree.SubElement(channel_element,'itunes:image', {'href' : link_prefix + yaml_data['image']})
+xml_tree.SubElement(channel_element,'language').text = yaml_data['language']
+xml_tree.SubElement(channel_element,'link').text = link_prefix
+
+
+
 
 
 
